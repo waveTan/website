689635ee087d16ae161f3f1fe0e859940de81128
Josh Welham <josh@aluminati.net>
2018-06-18 12:03:55 +0100

    mobile size 1 grid

--- client/src/views/pages/Index/HowItWorks.vue
+++ client/src/views/pages/Index/HowItWorks.vue
@@ -6,35 +6,35 @@
 				<h5>Technology & Advantages of NULS Ecosystem</h5>
 				<v-container grid-list-xl text-xs-center>
 					<v-layout row wrap>
-						<v-flex xs4>
+						<v-flex md4 xs12>
 							<h4>Multiple chains</h4>
 							<p>Exchange of data and value among chains</p>
 							<p>Extensive applications & better performance</p>
 						</v-flex>
-						<v-flex xs4>
+						<v-flex md4 xs12>
 							<h4>Smart contracts</h4>
 							<p>Less programming for defining service logic</p>
 							<p>Running login from NULSVM in a decentralized environment</p>
 						</v-flex>
-						<v-flex xs4>
+						<v-flex md4 xs12>
 							<h4>Proof-Of-Credit</h4>
 							<p>High scalability and rapid value transmission</p>
 							<p>Connection via cross-chain consensus modules</p>
 						</v-flex>
 					</v-layout>
 					<v-layout row wrap>
-						<v-flex xs2>&nbsp;</v-flex>
-						<v-flex xs4>
+						<v-flex md2 xs12>&nbsp;</v-flex>
+						<v-flex md4 xs12>
 							<h4>Customizable</h4>
 							<p>Modularization & multiple chains for higher flexibility</p>
 							<p>Selection of modules offer greater use cases</p>
 						</v-flex>
-						<v-flex xs4>
+						<v-flex md4 xs12>
 							<h4>Sustainable improvement</h4>
 							<p>Improving network technology, storage schemes and encryption algorithms</p>
 							<p>Independent upgradable modules for continuous improvement and scalability</p>
 						</v-flex>
-						<v-flex xs2>&nbsp;</v-flex>
+						<v-flex md2 xs12>&nbsp;</v-flex>
 					</v-layout>
 				</v-container>
 			</div>

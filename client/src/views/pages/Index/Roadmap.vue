7b841520217038bf070adb782a3c24a397542444
Josh Welham <josh@aluminati.net>
2018-06-21 09:23:57 +0100

    slider to work in mobile

--- client/src/views/pages/Index/Roadmap.vue
+++ client/src/views/pages/Index/Roadmap.vue
@@ -121,10 +121,25 @@
 		},
 		data()
 		{
+			let slidesPerView = 4;
+
+			if(window.innerWidth < 550)
+			{
+				slidesPerView = 1;
+			}
+			else if(window.innerWidth < 750)
+			{
+				slidesPerView = 2;
+			}
+			else if(window.innerWidth < 1000)
+			{
+				slidesPerView = 3;
+			}
+
 			return {
 				active: 'ice',
 				swiperOption: {
-					slidesPerView: 4,
+					slidesPerView,
 					spaceBetween: 30,
 					grabCursor: true,
 					pagination: {
@@ -217,6 +232,6 @@
 	}
 
 	.swiper-pagination {
-		bottom: -7px !important;
+		bottom: -4px !important;
 	}
 </style>

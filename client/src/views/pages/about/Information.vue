523ac0a7fb1546bc88b5d39a5d8c522c18f1d9ff
Josh Welham <josh@aluminati.net>
2018-06-22 21:38:33 +0100

    information section

--- client/src/views/pages/about/Information.vue
+++ client/src/views/pages/about/Information.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="section container">
-		Information Section
+		<div class="centered">
+			<h2><I18N id="page.about.information.title" /></h2>
+			<p><I18N id="page.about.information.meta" /></p>
+		</div>
 	</div>
 </template>

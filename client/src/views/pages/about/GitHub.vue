1f0746276e9ae85f1a1d5b3174406a16a6cc6aad
Josh Welham <josh@aluminati.net>
2018-06-22 21:38:15 +0100

    github section

--- client/src/views/pages/about/GitHub.vue
+++ client/src/views/pages/about/GitHub.vue
@@ -1,7 +1,18 @@
 <template>
 	<div class="section grey">
 		<div class="container">
-			GitHub Section
+			<div class="centered">
+				<h2><I18N id="page.about.github.title" /></h2>
+				<p><I18N id="page.about.github.meta" /></p>
+				<p><a href="https://github.com/nuls-io/" target="_blank"><I18N id="page.about.github.link" /></a></p>
+			</div>
 		</div>
 	</div>
 </template>
+
+<style scoped>
+	.container {
+		background: url('/static/images/github-background.png') no-repeat center;
+		min-height: 570px;
+	}
+</style>

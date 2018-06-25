6e3d8c459fa81b7e270650fe46525f683226c101
Josh Welham <josh@aluminati.net>
2018-06-25 17:23:35 +0100

    get base page contents

--- client/src/views/pages/Partners.vue
+++ client/src/views/pages/Partners.vue
@@ -1,8 +1,34 @@
 <template>
-	<div class="section container">
-		<div class="centered">
-			<h2>Hello world</h2>
-			<p>{{ getItems('platform') }}</p>
+	<div>
+		<div class="header">
+			<div class="container">
+				<h1><I18N id="page.partnerships.header.title" /></h1>
+				<p class="centered"><I18N id="page.partnerships.header.meta" /></p>
+			</div>
+		</div>
+		<div class="section">
+			<div class="container">
+				<h2 class="center"><I18N id="page.partnerships.cooperationProjects" /></h2>
+				<p>{{ getItems('cooperation projects') }}</p>
+			</div>
+		</div>
+		<div class="section grey">
+			<div class="container">
+				<h2 class="center"><I18N id="page.partnerships.cooperationMedia" /></h2>
+				<p>{{ getItems('cooperation media') }}</p>
+			</div>
+		</div>
+		<div class="section">
+			<div class="container">
+				<h2 class="center"><I18N id="page.partnerships.platforms" /></h2>
+				<p>{{ getItems('platform') }}</p>
+			</div>
+		</div>
+		<div class="section grey noClip">
+			<div class="container">
+				<h2 class="center"><I18N id="page.partnerships.becomeAPartner.title" /></h2>
+				<p></p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -23,7 +49,6 @@
 		methods: {
 			getItems(type)
 			{
-				console.log(this.partnerships);
 				return this.partnerships.filter((item) => item.type.toLowerCase() === type);
 			}
 		}

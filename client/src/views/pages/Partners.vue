96f8dce6e44368a596ba56e87ad4e6d15d5040e7
Josh Welham <josh@aluminati.net>
2018-06-25 15:42:36 +0100

    created page

---
+++ client/src/views/pages/Partners.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="section container">
+		<div class="centered">
+			<h2>Hello world</h2>
+			<p>Foo bar</p>
+		</div>
+	</div>
+</template>
+
+
+<script>
+	export default {
+
+	};
+</script>
+
+<style scoped>
+</style>

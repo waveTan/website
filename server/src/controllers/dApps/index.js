a9380a6eb2efe7666a99b35d49cf3d1e02d8cb57
Josh Welham <josh@aluminati.net>
2018-06-26 11:16:39 +0100

    load dapps list with pagination

--- server/src/controllers/dApps/index.js
+++ server/src/controllers/dApps/index.js
@@ -1,6 +1,23 @@
+const Database = require('@/models/Database');
+
 const load = async (req, res) =>
 {
-	res.status(200).json({ hello: 'world' });
+	const db = new Database();
+	const { offsetId } = req.params;
+	const limit = 2;
+	await db.init();
+
+	const [rows] = await db.connection.execute(`
+		SELECT d.id, d.title, d.description, d.link, d.active, u.url AS image
+		FROM dapps AS d
+		LEFT JOIN upload_file_morph AS m ON m.related_type = "partners" AND m.related_id = d.id
+		LEFT JOIN upload_file AS u ON m.upload_file_id = u.id
+		WHERE d.id < ?
+		ORDER BY d.id desc
+		LIMIT ?
+	`, [offsetId, limit]);
+
+	res.status(200).json(rows);
 };
 
 module.exports = {

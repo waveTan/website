2e0b5e2b4f99f8c3068b0cd5ac96e9484ad807c6
Josh Welham <josh@aluminati.net>
2018-06-26 16:04:07 +0100

    search query end point

--- server/src/controllers/dApps/index.js
+++ server/src/controllers/dApps/index.js
@@ -1,6 +1,6 @@
 const Database = require('@/models/Database');
 
-const resultsLimit = 5;
+const resultsLimit = 2;
 
 const load = async (req, res) =>
 {
@@ -13,7 +13,11 @@ const load = async (req, res) =>
 		FROM dapps AS d
 		LEFT JOIN upload_file_morph AS m ON m.related_type = "dapps" AND m.related_id = d.id
 		LEFT JOIN upload_file AS u ON m.upload_file_id = u.id
-		WHERE CASE WHEN 0 != ? THEN d.id < ? ELSE 1=1 END
+		WHERE
+		CASE
+			WHEN 0 != ? THEN id < ?
+			ELSE 1=1
+		END
 		ORDER BY d.id desc
 		LIMIT ?
 	`, [offsetId, offsetId, resultsLimit]);
@@ -21,6 +25,30 @@ const load = async (req, res) =>
 	res.status(200).json(rows);
 };
 
+const search = async (req, res) =>
+{
+	const db = new Database();
+	const { offsetId = 0 } = req.params;
+	const { query } = req.body;
+	await db.init();
+
+	const [rows] = await db.connection.execute(`
+		SELECT d.id, d.title, d.description, d.link, d.active
+		FROM dapps AS d
+		WHERE
+		CASE
+			WHEN 0 != ? THEN id < ?
+			ELSE 1=1
+		END
+		AND MATCH (d.title, d.description) AGAINST (? IN BOOLEAN MODE)
+		ORDER BY d.id desc
+		LIMIT ?
+	`, [offsetId, offsetId, query, resultsLimit]);
+
+	res.status(200).json(rows);
+};
+
 module.exports = {
-	load
+	load,
+	search
 };
--- server/src/routes/api/v1/dApps/index.js
+++ server/src/routes/api/v1/dApps/index.js
@@ -4,6 +4,8 @@ const dApps = require('@/controllers/dApps');
 const router = express.Router();
 
 router.get('/', dApps.load);
+router.get('/search/', dApps.search);
+router.get('/search/:offsetId', dApps.search);
 router.get('/:offsetId', dApps.load);
 
 module.exports = router;

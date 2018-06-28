bf6ea0aab6783f337651a15fd0da28caa0c01254
Josh Welham <josh@aluminati.net>
2018-06-28 09:48:12 +0100

    moved expansion panel into one file using slots

---
+++ client/src/components/vuetify/ExpansionPanel.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-expansion-panel>
+		<v-expansion-panel-content v-for="(item,i) in 5" :key="i">
+			<div slot="header"><slot name="header" /></div>
+			<v-card>
+				<v-card-text>
+					<v-card-text>
+						22<slot />
+					</v-card-text>
+					<v-card-text class="footer">
+						33<slot name="footer" />
+					</v-card-text>
+				</v-card-text>
+			</v-card>
+		</v-expansion-panel-content>
+	</v-expansion-panel>
+</template>
+
+<script>
+	export default {
+	};
+</script>
+
+<style scoped>
+	.footer {
+		opacity: 0.6;
+	}
+</style>

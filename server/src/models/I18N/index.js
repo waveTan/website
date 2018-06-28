b743a0da73ffdcb853112a7f79d3d11754486f7b
Josh Welham <josh@aluminati.net>
2018-06-28 21:00:44 +0100

    function to transform query results into users language

--- server/src/models/I18N/index.js
+++ server/src/models/I18N/index.js
@@ -24,6 +24,31 @@ class I18N
 	{
 		return Data.read(`i18n/${fileName}`);
 	}
+
+	static transformQueryResults(rows, locale)
+	{
+		rows.map((row) =>
+		{
+			const columns = Object.keys(row).filter((column) => column.substr(0, 3) === 'en_');
+
+			columns.forEach((column) =>
+			{
+				if(`${locale}_${column.substr(3)}` && row[`${locale}_${column.substr(3)}`])
+				{
+					row[column.substr(3)] = row[`${locale}_${column.substr(3)}`];
+				}
+				else
+				{
+					row[column.substr(3)] = row[column];
+				}
+
+				delete row[column];
+				delete row[`zh_${column.substr(3)}`];
+			});
+
+			return row;
+		});
+	}
 }
 
 module.exports = I18N;

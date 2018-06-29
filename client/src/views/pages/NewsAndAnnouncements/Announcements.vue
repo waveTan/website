dd13599fabd64539e92e946d11b153eec39113c0
Josh Welham <josh@aluminati.net>
2018-06-29 16:59:29 +0100

    announcements list items

--- client/src/views/pages/NewsAndAnnouncements/Announcements.vue
+++ client/src/views/pages/NewsAndAnnouncements/Announcements.vue
@@ -1,6 +1,43 @@
 <template>
 	<div>
-		<HeaderMeta title="page.newsAndAnnouncements.pageTitle.announcements" />
-		Announcements component....
+		<HeaderMeta title="page.announcements.pageTitle" />
+		<ItemsPro itemType="announcements">
+			<template slot-scope="{ item, imageDirectory }">
+				<a :href="item.link" target="_blank">
+					<v-card-media
+							:src="`${imageDirectory}${item.image}`"
+							height="200px"
+					/>
+					<v-card-title primary-title>
+						<div>
+							<h4 class="headline mb-0 center">{{ item.title }}</h4>
+							<p class="center">{{ item.description }}</p>
+						</div>
+					</v-card-title>
+				</a>
+			</template>
+		</ItemsPro>
 	</div>
 </template>
+
+<script>
+	import ItemsPro from '@/components/ItemsPro';
+
+	export default {
+		components: {
+			ItemsPro
+		}
+	}
+</script>
+
+<style>
+	.container.announcementsItems .card__media {
+		background: #fff;
+		box-shadow: 0 8px 24px 0 rgba(186, 194, 198, 0.5), 0 3px 6px 0 rgba(186, 194, 198, 0.2);
+		border-radius: 6px;
+	}
+
+	.container.announcementsItems .card__title div {
+		width: 100%;
+	}
+</style>

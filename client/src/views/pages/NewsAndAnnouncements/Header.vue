9070c1b6e0da62a71a29fe63ed68eb2f964df865
Josh Welham <josh@aluminati.net>
2018-06-29 10:59:36 +0100

    header styling

--- client/src/views/pages/NewsAndAnnouncements/Header.vue
+++ client/src/views/pages/NewsAndAnnouncements/Header.vue
@@ -3,8 +3,10 @@
 		<div class="container">
 			<h1 v-if="category === 'news'"><I18N id="page.newsAndAnnouncements.header.news" /></h1>
 			<h1 v-else><I18N id="page.newsAndAnnouncements.header.announcements" /></h1>
-			<router-link :to="{ name: 'news' }">News</router-link>
-			<router-link :to="{ name: 'announcements' }">Announcements</router-link>
+			<ul class="centered">
+				<li :class="{ active: category === 'news' }"><router-link :to="{ name: 'news' }">News</router-link></li>
+				<li :class="{ active: category === 'announcements' }"><router-link :to="{ name: 'announcements' }">Announcements</router-link></li>
+			</ul>
 		</div>
 	</div>
 </template>
@@ -30,6 +32,36 @@
 	}
 
 	.section.grey .centered {
-		padding: 0;
+		padding: 20px 0 140px 0;
+	}
+
+	ul {
+		list-style: none;
+		width: fit-content;
+	}
+
+	li {
+		float: left;
+		border-radius: 100px;
+		font-size: 24px;
+		letter-spacing: 1px;
+		line-height: 76.8px;
+		cursor: pointer;
+	}
+
+	a {
+		border: none;
+		display: block;
+		padding: 0 30px;
+	}
+
+	li.active,
+	li:hover {
+		background: #fff;
+		box-shadow: 0 8px 24px 0 rgba(186, 194, 198, 0.5), 0 3px 6px 0 rgba(186, 194, 198, 0.2);
+	}
+
+	li.active a {
+		color: #56c400;
 	}
 </style>

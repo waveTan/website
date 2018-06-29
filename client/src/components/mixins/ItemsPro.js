0be46b2e28ae407cffa26b7574c1a60f9a731074
Josh Welham <josh@aluminati.net>
2018-06-29 16:04:28 +0100

    moved into a mixin

---
+++ client/src/components/mixins/ItemsPro.js
@@ -0,0 +1,99 @@
+import debounce from 'lodash/debounce';
+
+export default {
+	data()
+	{
+		return {
+			itemType: 'REPLACE_ME',
+			searchQuery: '',
+			page: 1,
+			items: [],
+			totalItems: 1,
+			totalPages: 1,
+			imageDirectory: this.$store.getters['items/strapiUrl']
+		};
+	},
+	computed: {
+		loading()
+		{
+			return this.$store.getters['itemsPro/getLoading'];
+		},
+		getItems()
+		{
+			if(this.searchQuery === '')
+			{
+				if(!this.$store.getters['itemsPro/getItems'](this.itemType, this.page))
+				{
+					this.loadItems();
+				}
+
+				const data = this.$store.getters['itemsPro/getItems'](this.itemType, this.page);
+
+				this.setItemsListData(data);
+
+				return this.items;
+			}
+
+			if(!this.$store.getters['itemsPro/getSearchItems'](this.page))
+			{
+				this.searchItems();
+			}
+
+			return this.$store.getters['itemsPro/getSearchItems'](this.page);
+		}
+	},
+	methods: {
+		async loadItems()
+		{
+			if(this.searchQuery !== '') return;
+
+			const data = await this.$store.dispatch('itemsPro/loadItems', { type: this.itemType, page: this.page });
+
+			this.setItemsListData(data);
+		},
+		async searchItems()
+		{
+			const data = await this.$store.dispatch('itemsPro/search', { type: this.itemType, searchQuery: this.searchQuery, page: this.page });
+
+			this.setItemsListData(data);
+		},
+		inputUpdated: debounce(async function run()
+		{
+			this.page = 1;
+			this.totalItems = 1;
+			this.totalPages = 1;
+
+			if(this.searchQuery === '')
+			{
+				this.loadItems();
+
+				return;
+			}
+
+			this.searchItems();
+		}, 500),
+		pageUpdated()
+		{
+			if(this.searchQuery === '')
+			{
+				this.loadItems();
+			}
+			else
+			{
+				this.searchItems();
+			}
+		},
+		setItemsListData(data)
+		{
+			if(!data) return;
+
+			if(data.count)
+			{
+				this.totalItems = data.count;
+				this.totalPages = Math.ceil(this.totalItems / this.$store.getters['itemsPro/itemsPerPage']);
+			}
+
+			this.items = data.rows;
+		}
+	}
+};
--- client/src/views/pages/dApps/Items.vue
+++ client/src/views/pages/dApps/Items.vue
@@ -49,101 +49,16 @@
 </template>
 
 <script>
-	import debounce from 'lodash/debounce';
+	import ItemsPro from '@/components/mixins/ItemsPro';
 
 	export default {
 		data()
 		{
 			return {
-				itemType: 'dApps',
-				searchQuery: '',
-				page: 1,
-				items: [],
-				totalItems: 1,
-				totalPages: 1,
-				imageDirectory: this.$store.getters['items/strapiUrl']
-			};
-		},
-		computed: {
-			loading()
-			{
-				return this.$store.getters['itemsPro/getLoading'];
-			},
-			getItems()
-			{
-				if(this.searchQuery === '')
-				{
-					if(!this.$store.getters['itemsPro/getItems'](this.itemType, this.page))
-					{
-						this.loadItems();
-					}
-
-					const data = this.$store.getters['itemsPro/getItems'](this.itemType, this.page);
-
-					this.setItemsListData(data);
-
-					return this.items;
-				}
-
-				if(!this.$store.getters['itemsPro/getSearchItems'](this.page))
-				{
-					this.searchItems();
-				}
-
-				return this.$store.getters['itemsPro/getSearchItems'](this.page);
+				itemType: 'dApps'
 			}
 		},
-		methods: {
-			async loadItems()
-			{
-				if(this.searchQuery !== '') return;
-
-				const data = await this.$store.dispatch('itemsPro/loadItems', { type: this.itemType, page: this.page });
-
-				this.setItemsListData(data);
-			},
-			async searchItems()
-			{
-				const data = await this.$store.dispatch('itemsPro/search', { searchQuery: this.searchQuery, page: this.page });
-
-				this.setItemsListData(data);
-			},
-			inputUpdated: debounce(async function run()
-			{
-				this.page = 1;
-				this.totalItems = 1;
-				this.totalPages = 1;
-
-				if(this.searchQuery === '')
-				{
-					this.loadItems();
-					return;
-				}
-
-				this.searchItems();
-			}, 500),
-			pageUpdated()
-			{
-				if(this.searchQuery === '')
-				{
-					this.loadItems();
-				}
-				else
-				{
-					this.searchItems();
-				}
-			},
-			setItemsListData(data)
-			{
-				if(data.count)
-				{
-					this.totalItems = data.count;
-					this.totalPages = Math.ceil(this.totalItems / this.$store.getters['itemsPro/itemsPerPage']);
-				}
-
-				this.items = data.rows;
-			}
-		}
+		mixins: [ItemsPro]
 	}
 </script>
 

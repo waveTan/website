7a0d2545a9407e1b545a2d2dc98e09f147d21edc
Josh Welham <josh@aluminati.net>
2018-07-05 21:03:31 +0100

    mixin isnt needed anymore

--- client/src/components/ItemsPro.vue
+++ client/src/components/ItemsPro.vue
@@ -47,10 +47,9 @@
 </template>
 
 <script>
-	import ItemsPro from '@/components/mixins/ItemsPro';
+	import debounce from 'lodash/debounce';
 
 	export default {
-		mixins: [ItemsPro],
 		props: {
 			itemType: {
 				type: String,
@@ -69,10 +68,102 @@
 				default: false
 			}
 		},
+		data()
+		{
+			return {
+				searchQuery: '',
+				page: 1,
+				items: [],
+				totalItems: 1,
+				totalPages: 1,
+				imageDirectory: this.$store.getters['items/strapiUrl']
+			};
+		},
 		computed: {
 			searchTextKey()
 			{
 				return `page.${this.itemType}.items.search`;
+			},
+			loading()
+			{
+				return this.$store.getters['itemsPro/getLoading'];
+			},
+			getItems()
+			{
+				if(this.searchQuery === '')
+				{
+					if(!this.$store.getters['itemsPro/getItems'](this.itemType, this.page))
+					{
+						this.loadItems();
+					}
+
+					const data = this.$store.getters['itemsPro/getItems'](this.itemType, this.page);
+
+					this.setItemsListData(data);
+
+					return this.items;
+				}
+
+				if(!this.$store.getters['itemsPro/getSearchItems'](this.page))
+				{
+					this.searchItems();
+				}
+
+				return this.$store.getters['itemsPro/getSearchItems'](this.page);
+			}
+		},
+		methods: {
+			async loadItems()
+			{
+				if(this.searchQuery !== '') return;
+
+				const data = await this.$store.dispatch('itemsPro/loadItems', { type: this.itemType, page: this.page });
+
+				this.setItemsListData(data);
+			},
+			async searchItems()
+			{
+				const data = await this.$store.dispatch('itemsPro/search', { type: this.itemType, searchQuery: this.searchQuery, page: this.page });
+
+				this.setItemsListData(data);
+			},
+			inputUpdated: debounce(async function run()
+			{
+				this.page = 1;
+				this.totalItems = 1;
+				this.totalPages = 1;
+
+				if(this.searchQuery === '')
+				{
+					this.loadItems();
+
+					return;
+				}
+
+				this.searchItems();
+			}, 500),
+			pageUpdated()
+			{
+				if(this.searchQuery === '')
+				{
+					this.loadItems();
+				}
+				else
+				{
+					this.searchItems();
+				}
+			},
+			setItemsListData(data)
+			{
+				if(!data) return;
+
+				if(data.count)
+				{
+					this.totalItems = data.count;
+					this.totalPages = Math.ceil(this.totalItems / this.$store.getters['itemsPro/itemsPerPage']);
+				}
+
+				this.items = data.rows;
 			}
 		}
 	};

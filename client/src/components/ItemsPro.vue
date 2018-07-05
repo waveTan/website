84ec060d26bb3ea86a4c062b91d67093e2bcbbce
Josh Welham <josh@aluminati.net>
2018-07-05 21:01:53 +0100

    cleaner code

--- client/src/components/ItemsPro.vue
+++ client/src/components/ItemsPro.vue
@@ -1,19 +1,33 @@
 <template>
 	<div class="section container">
 		<v-text-field
-				class="searchInput"
-				v-model="searchQuery"
-				append-icon="search"
-				:label="`${$t(searchTextKey)}`"
-				@input="inputUpdated"
+			v-model="searchQuery"
+			:label="`${$t(searchTextKey)}`"
+			append-icon="search"
+			class="searchInput"
+			@input="inputUpdated"
 		/>
 		<div v-if="loading" class="center">
-			<v-progress-circular :size="50" indeterminate color="primary" />
+			<v-progress-circular
+				:size="50"
+				indeterminate
+				color="primary"
+			/>
 		</div>
 		<div v-else>
-			<v-container grid-list-md text-xs-center :class="`${itemType}Items`">
+			<v-container
+				:class="`${itemType}Items`"
+				grid-list-md
+				text-xs-center
+			>
 				<v-layout row wrap>
-					<v-flex v-for="(item, i) in getItems" :key="i" :sm12="sm12" :sm6="sm6" :md3="md3">
+					<v-flex
+						v-for="(item, i) in getItems"
+						:key="i"
+						:sm12="sm12"
+						:sm6="sm6"
+						:md3="md3"
+					>
 						<v-card flat tile>
 							<slot :item="item" :imageDirectory="imageDirectory" />
 						</v-card>
@@ -36,6 +50,7 @@
 	import ItemsPro from '@/components/mixins/ItemsPro';
 
 	export default {
+		mixins: [ItemsPro],
 		props: {
 			itemType: {
 				type: String,
@@ -54,12 +69,11 @@
 				default: false
 			}
 		},
-		mixins: [ItemsPro],
 		computed: {
 			searchTextKey()
 			{
 				return `page.${this.itemType}.items.search`;
 			}
 		}
-	}
+	};
 </script>

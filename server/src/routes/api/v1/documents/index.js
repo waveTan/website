ea3a627b2f7b4a70b9c9d6c832e556d84b9b6348
Josh Welham <josh@aluminati.net>
2018-07-06 19:08:58 +0100

    added documents api backend

---
+++ server/src/routes/api/v1/documents/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const documents = require('@/controllers/documents');
+
+const router = express.Router();
+
+router.get('/', documents.load);
+router.post('/search/', documents.search);
+router.post('/search/:offsetId', documents.search);
+router.get('/item/:id', documents.item);
+router.get('/:offsetId', documents.load);
+
+module.exports = router;

e1fa61a2a25170c0b4da1e3d7d549c3f13d74543
daviyang35 <daviyang35@gmail.com>
2018-07-07 22:45:22 +0800

    use env variables

--- client/build/config.js
+++ client/build/config.js
@@ -3,8 +3,8 @@ module.exports = {
 	// Options for webpack-dev-server
 	// See https://webpack.js.org/configuration/dev-server
 	devServer: {
-		host: 'localhost',
-		port: 4000
+		host: "${process.env.HOST || 'localhost' }",
+		port: "${process.env.PORT || 4000}",
 	},
 	// when you use electron please set to relative path like ./
 	// otherwise only set to absolute path when you're using history mode

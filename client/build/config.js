6e4bd5d5c1770cf9702fccbe83addb3de41a12cc
Josh Welham <josh@aluminati.net>
2018-07-09 18:44:39 +0100

    added back static folder

--- client/build/config.js
+++ client/build/config.js
@@ -6,13 +6,13 @@ module.exports = {
 	// See https://webpack.js.org/configuration/dev-server
 	devServer: {
 		host: process.env.HOST || 'localhost',
-		port: process.env.PORT || 4000,
+		port: process.env.PORT || 4000
 	},
 	// when you use electron please set to relative path like ./
 	// otherwise only set to absolute path when you're using history mode
 	publicPath: './',
 	electron: false,
-	assetsSubDirectory: 'src/assets',
+	assetsSubDirectory: 'static',
 	assetsPublicPath: '/',
 	useEslint: true,
 	showEslintErrorsInOverlay: true

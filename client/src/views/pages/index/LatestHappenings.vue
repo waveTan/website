42448398b633d13d68566c8e004f01c7f8678390
Josh Welham <josh@aluminati.net>
2018-08-03 23:20:48 +0100

    ensure all card titles are the same height

--- client/src/views/pages/index/LatestHappenings.vue
+++ client/src/views/pages/index/LatestHappenings.vue
@@ -10,11 +10,11 @@
 		</div>
 		<div v-else class="container">
 			<swiper :options="swiperOption">
-				<swiper-slide v-for="(item, i) in getItems" :key="i">
+				<swiper-slide v-for="(item, i) in getItems" :key="i" ref="items">
 					<router-link :to="{ name: 'newsItem', params: { id: item.id, title: item.title } }">
 						<v-card>
 							<v-card-media :src="`${imageDirectory}${item.image}`" height="180px" />
-							<v-card-title primary-title>
+							<v-card-title :style="cardTitleHeight" primary-title>
 								<h5>{{ item.title }}</h5>
 							</v-card-title>
 						</v-card>
@@ -24,7 +24,7 @@
 					<router-link :to="{ name: 'news' }">
 						<v-card>
 							<v-card-media class="more" height="180px" />
-							<v-card-title primary-title>
+							<v-card-title :style="cardTitleHeight" primary-title>
 								<h5><I18N id="page.index.latestHappenings.more" /></h5>
 							</v-card-title>
 						</v-card>
@@ -50,8 +50,24 @@
 		{
 			return {
 				swiperOption: swiperOption(),
-				imageDirectory: this.$store.getters['items/strapiUrl']
+				imageDirectory: this.$store.getters['items/strapiUrl'],
+				cardTitleHeight: ''
 			};
+		},
+		updated()
+		{
+			if(!this.$refs.items) return;
+
+			let tallestItem = 0;
+
+			this.$refs.items.forEach((item) => (tallestItem = tallestItem < item.$el.offsetHeight ? item.$el.offsetHeight : tallestItem));
+
+			const newHeight = `height: ${tallestItem - 180}px; align-items: normal;`;
+
+			if(this.cardTitleHeight === '')
+			{
+				this.cardTitleHeight = newHeight;
+			}
 		},
 		computed: {
 			pageLoading()

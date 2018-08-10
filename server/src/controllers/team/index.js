5e93072153c56191a28e984532ff6ca1adb3aeac
Josh Welham <josh@aluminati.net>
2018-08-10 09:10:58 +0100

    removed debug

--- server/src/controllers/team/index.js
+++ server/src/controllers/team/index.js
@@ -16,8 +16,7 @@ const getEverything = async (req, res) =>
 			t.zh_content,
 			t.linkedIn,
 			t.category,
-			u.url AS image,
-			t.serialNumber
+			u.url AS image
 		FROM teams AS t
 		LEFT JOIN upload_file_morph AS m ON m.related_type = "teams" AND m.related_id = t.id
 		LEFT JOIN upload_file AS u ON m.upload_file_id = u.id
